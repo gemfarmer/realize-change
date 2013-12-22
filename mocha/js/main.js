54a4691ef945433ce65ce995281d982a1f2d9686
Michael Herman <hermanmu@gmail.com>
2013-12-21 23:03:44 -0600

    updated test folder:

--- mocha/js/main.js
+++
@@ -1,7 +0,0 @@
-/*
- * A trivial snippet of code to put under test.
- */
-function appendDiv(selector) {
-    var element = document.querySelector(selector);
-    return element.appendChild(document.createElement('div'));
-}
